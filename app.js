93d83bf0ce4f77e86f9bd8d173154142b785252b
Felicia Chen <feliciachen18@gmail.com>
2023-04-23 16:42:22 -0700

    working on styling

--- app.js
+++ app.js
@@ -16,11 +16,38 @@ menu.addEventListener('click', function() {
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+const scrollUp = () => {
+    const scrollup = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== DARK LIGHT THEME ===============*/ 
+
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
